7aaee1db3633c0e309f7df01aaa4b1b19bc00bb5
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-23 05:35:08 +0900

    Update DarkModeToggle.vue

--- src/components/ui/DarkModeToggle.vue
+++ src/components/ui/DarkModeToggle.vue
@@ -35,24 +35,22 @@ export default {
 </script>
 
 <style scoped>
-.dark-mode-switch {
-  :deep(.el-switch__core) {
-    min-width: 3.5rem;
-    box-shadow: inset 0 4px 6px 2px rgba(225, 0, 0, 0.5);
-    --el-switch-off-color: #ffbf0e;
-  }
-  
-  :deep(.el-switch.is-checked .el-switch__core) {
-    box-shadow: inset 0 4px 6px 2px rgba(0, 0, 225, 0.5);
-    --el-switch-on-color: #0081ec;
-  }
-  
-  :deep(.el-switch__action) {
-    box-shadow: 0px 0px 4px 1px rgba(225, 0, 0, 0.25);
-  }
-  
-  :deep(.el-switch.is-checked .el-switch__action) {
-    box-shadow: 0px 0px 4px 1px rgba(0, 0, 225, 0.25);
-  }
+.dark-mode-switch :deep(.el-switch__core) {
+  min-width: 3.5rem;
+  box-shadow: inset 0 4px 6px 2px rgba(225, 0, 0, 0.5);
+  --el-switch-off-color: #ffbf0e;
+}
+
+.dark-mode-switch :deep(.el-switch.is-checked .el-switch__core) {
+  box-shadow: inset 0 4px 6px 2px rgba(0, 0, 225, 0.5);
+  --el-switch-on-color: #0081ec;
+}
+
+.dark-mode-switch :deep(.el-switch__action) {
+  box-shadow: 0px 0px 4px 1px rgba(225, 0, 0, 0.25);
+}
+
+.dark-mode-switch :deep(.el-switch.is-checked .el-switch__action) {
+  box-shadow: 0px 0px 4px 1px rgba(0, 0, 225, 0.25);
 }
 </style>
