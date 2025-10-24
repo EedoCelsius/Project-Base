862894cd688ffe6b8160f292a5ccc8b08ba276ea
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-25 06:31:24 +0900

    Rename stored locale ref

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,13 +1,25 @@
 import { createI18n } from 'vue-i18n';
+import { watch } from 'vue';
+import { useStorage } from '@vueuse/core';
+
 import en from '@/locales/en.json';
 import ko from '@/locales/ko.json';
 
-export default createI18n({
+const browserLocale = navigator.language?.split('-')[0] ?? 'en';
+const recentLocale = useStorage('locale', browserLocale);
+
+const i18n = createI18n({
   legacy: false,
-  locale: 'en',
+  locale: recentLocale.value,
   fallbackLocale: 'en',
   messages: {
     en,
     ko
   }
 });
+
+watch(i18n.global.locale, (newLocale) => {
+  recentLocale.value = newLocale;
+});
+
+export default i18n;
