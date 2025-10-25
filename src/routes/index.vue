8825bfc09848cab546ffaa65450d604606435d80
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-25 21:17:18 +0900

    Replace AppHeader component with Header component

--- src/routes/index.vue
+++ src/routes/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="px-6 py-8">
     <header class="mx-auto mb-10 w-full max-w-5xl">
-      <AppHeader />
+      <Header />
     </header>
     <main class="mx-auto w-full max-w-5xl space-y-6">
       <RouterView />
