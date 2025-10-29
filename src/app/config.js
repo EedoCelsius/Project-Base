2f083b6279172573db8dda888fcc265dc8a8f349
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-29 13:01:41 +0900

    Adjust router builder to accept config objects

--- src/app/config.js
+++ src/app/config.js
@@ -1,33 +1,23 @@
-import primeVueConfig from './primevue/config';
-import elementPlusConfig from './element-plus/config';
-import customTailwindConfig from './custom-tailwind/config';
-
 export default {
   routes: [
     {
-      path: 'primevue',
+      src: './primevue',
       alias: '',
-      component: () => import('./primevue/index.vue'),
       meta: {
         title: 'PrimeVue'
-      },
-      children: primeVueConfig.routes
+      }
     },
     {
-      path: 'element-plus',
-      component: () => import('./element-plus/index.vue'),
+      src: './element-plus',
       meta: {
         title: 'Element Plus'
-      },
-      children: elementPlusConfig.routes
+      }
     },
     {
-      path: 'custom-tailwind',
-      component: () => import('./custom-tailwind/index.vue'),
+      src: './custom-tailwind',
       meta: {
         title: 'Custom Tailwind'
-      },
-      children: customTailwindConfig.routes
+      }
     }
   ]
 };
--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,8 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { useTitle } from '@vueuse/core';
+import path from 'path';
 import i18n from '@/plugins/i18n';
 
 import appConfig from '@/app/config';
+
+const routeConfigs = import.meta.glob('/src/app/**/config.js', { eager: true });
+const routeComponents = import.meta.glob('/src/app/**/index.vue');
+
+const buildRoutes = (configs = {}, currentDir) =>
+  (configs.routes ?? []).map(({ src, ...route }) => {
+    const routeDir = path.posix.join(currentDir, src);
+    const componentPath = path.posix.join(routeDir, 'index.vue');
+    const configPath = path.posix.join(routeDir, 'config.js');
+
+    const childrenConfig = routeConfigs[configPath]?.default;
+
+    return {
+      ...route,
+      path: path.posix.basename(routeDir),
+      component: routeComponents[componentPath],
+      children: buildRoutes(childrenConfig, routeDir)
+    };
+  });
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,7 +30,7 @@ const router = createRouter({
     {
       path: '/',
       component: () => import('@/app/index.vue'),
-      children: appConfig.routes
+      children: buildRoutes(appConfig, '/src/app')
     }
   ]
 });
