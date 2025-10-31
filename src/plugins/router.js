afccbc7abd8ef8778e6c5ac18629dbbc82686330
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-31 09:35:59 +0900

    Refactor router to improve route handling and titles

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -3,44 +3,43 @@ import { useTitle } from '@vueuse/core';
 import path from 'path';
 import i18n from '@/plugins/i18n';
 
+const APP_DIR = '/src/app'
 const configs = import.meta.glob('/src/app/**/config.json', { eager: true });
 const components = import.meta.glob('/src/app/**/index.vue');
 
 const loadConfig = (dir) => configs[path.posix.join(dir, 'config.json')]?.default ?? {};
 
-const buildRoutes = (dir, routes = []) =>
-  routes.map((routeConfig) => {
-    const { src, ...route } =
-      typeof routeConfig === 'string' ? { src: routeConfig } : routeConfig;
+const buildRoutes = (dir, routes) =>
+  routes?.map((route) => {
+    if (typeof route === 'string') route = { src: route }
+    const { src, ...rest } = route;
 
     const subDir = path.posix.join(dir, src);
     const config = loadConfig(subDir);
-    const component = components[path.posix.join(subDir, 'index.vue')];
 
     return {
-      component,
-      path: path.posix.basename(path.posix.relative('/src/app', subDir)),
+      component: components[path.posix.join(subDir, 'index.vue')],
+      path: path.posix.basename(path.posix.relative(APP_DIR, subDir)),
       children: buildRoutes(subDir, config.routes),
       meta: config.meta,
-      ...route
+      ...rest
     };
   });
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: buildRoutes('/src/app', [''])
+  routes: buildRoutes(APP_DIR, ['/'])
 });
 
 const title = useTitle();
+const siteTitle = i18n.global.t('header.title');
+const locales = [i18n.global.locale.value, ...i18n.global.fallbackLocale.value];
 
 router.afterEach((to) => {
-  const baseTitle = i18n.global.t('header.title');
-
   let pageTitle = to.meta?.title;
-  const locales = [i18n.global.locale.value, ...i18n.global.fallbackLocale.value];
   if (typeof pageTitle === 'object') pageTitle = locales.map((locale) => pageTitle[locale]).find(Boolean);
 
-  title.value = pageTitle ? `${pageTitle} | ${baseTitle}` : baseTitle;
+  title.value = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle;
 });
 
 export default router;
