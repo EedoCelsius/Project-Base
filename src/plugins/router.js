3f81d1cfdb052787495c3d1f3481e7afee9055e1
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-11-01 12:52:24 +0900

    Update router.js

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -9,27 +9,30 @@ const configs = import.meta.glob('/src/app/**/config.json', { eager: true });
 const components = import.meta.glob('/src/app/**/index.vue');
 
 const loadConfig = (dir) => configs[path.posix.join(dir, 'config.json')]?.default ?? {};
+const rootConfig = loadConfig(APP_DIR);
 
-const buildRoutes = (dir, routes) =>
-  routes?.map((route) => {
-    if (typeof route === 'string') route = { src: route }
-    const { src, ...rest } = route;
-
-    const subDir = path.posix.join(dir, src);
-    const config = loadConfig(subDir);
+const buildRoutes = (dir, opts = {}) => {
+    const { meta, routes } = loadConfig(dir);
 
     return {
-      component: components[path.posix.join(subDir, 'index.vue')],
-      path: path.posix.basename(path.posix.relative(APP_DIR, subDir)),
-      children: buildRoutes(subDir, config.routes),
-      meta: config.meta,
-      ...rest
+        meta,
+        component: components[path.posix.join(dir, 'index.vue')],
+        path: path.posix.basename(path.posix.relative(APP_DIR, dir)),
+        children: routes?.map((route) => {
+            if (typeof route === 'string') route = { src: route }
+
+            const { src, ...subOpts } = route;
+            const subDir = path.posix.join(dir, src)
+
+            return buildRoutes(subDir, subOpts)
+        }),
+        ...opts
     };
-  });
+};
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: buildRoutes(APP_DIR, ['/'])
+  routes: buildRoutes(APP_DIR)
 });
 
 const title = useTitle();
@@ -39,12 +42,10 @@ const resolveTitle = (value) => {
   return locales.map((locale) => value[locale]).find(Boolean);
 };
 
-const rootConfig = loadConfig(APP_DIR);
-
 watch(
   [router.currentRoute, i18n.global.locale],
   ([route]) => {
-    const appTitle = resolveTitle(rootConfig.meta.title);
+    const appTitle = resolveTitle(rootConfig.meta?.title);
     const pageTitle = resolveTitle(route.meta?.title);
 
     title.value = pageTitle !== appTitle ? `${pageTitle} | ${appTitle}` : appTitle;
