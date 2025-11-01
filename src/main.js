4213945e4f4a673737aad9e3cd7645686ba864a8
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-11-01 16:57:33 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+import { createApp, h } from 'vue';
 import { createPinia } from 'pinia';
 import PrimeVue from 'primevue/config';
 import ElementPlus from 'element-plus';
@@ -6,12 +6,11 @@ import ElementPlus from 'element-plus';
 import i18n from './plugins/i18n';
 import primevueOptions from './plugins/primevue';
 import router from './plugins/router';
-import App from './app/index.vue';
 
 import 'primeicons/primeicons.css';
 import './styles/index.css';
 
-const app = createApp(App);
+const app = createApp({ render: () => h(RouterView) });
 app.use(createPinia());
 app.use(PrimeVue, primevueOptions);
 app.use(ElementPlus);
